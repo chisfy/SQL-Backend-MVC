02e83349e0b970cdd50a914af75a049ebeffca49
chisfy <fyzac96@gmail.com>
2023-10-09 19:21:29 +0100

    comments added to models and crud update functions added

--- models/dogmodel.js
+++ models/dogmodel.js
@@ -1,7 +1,7 @@
 import { pool } from "../db/connection.js";
 
 export async function getDogs() {
-  // Query the database and return all books
+  // Query the database and return all dogs
 
   // Define the SQL query to get all dogs from the 'doga' table
   const querySQLText = "SELECT * FROM dogs";
@@ -14,12 +14,12 @@ export async function getDogs() {
 }
 
 export async function getDogByID(id) {
-  // Query the database and return all books
-
-  // Define the SQL query to get all dogs from the 'doga' table
+  //Query the database and find an owner by theri id in the path url
+  
+  // Define the SQL query to get a specific owner from the 'owners' table (paramertized query)
   const querySQLText = "SELECT * FROM dogs WHERE dog_id = $1";
 
-  // Use the pool object to send the query to the database
+  // Use the pool object to send the query to the database (paramertized query)
   const result = await pool.query(querySQLText, [id]);
 
   // The rows property of the result object contains the retrieved record or null if not found
@@ -27,9 +27,9 @@ export async function getDogByID(id) {
 }
 
 export async function getDogsAlphabetical() {
-  // Query the database and return all books
+  // Query the database and return all dogs in alphabetical order
 
-  // Define the SQL query to get all dogs from the 'doga' table
+  // Define the SQL query to get all dogs from the 'dogs' table order by name ascending
   const querySQLText = "SELECT * FROM dogs ORDER BY name ASC";
 
   // Use the pool object to send the query to the database
@@ -40,12 +40,12 @@ export async function getDogsAlphabetical() {
 }
 
 export async function getDogsByName(name) {
-  // Query the database and return all books
+  // Query the database and all dogs under that name in the url
 
-  // Define the SQL query to get all dogs from the 'doga' table
+  // Define the SQL query to get all dogs from the 'dogs' table (paramertized query)
   const querySQLText = "SELECT * FROM dogs WHERE name = $1 ORDER BY age DESC";
 
-  // Use the pool object to send the query to the database
+  // Use the pool object to send the query to the database (paramertized query)
   const result = await pool.query(querySQLText, [name]);
 
   // The rows property of the result object contains the retrieved record or null if not found
@@ -53,9 +53,9 @@ export async function getDogsByName(name) {
 }
 
 export async function deleteDogByID(id) {
-  // Query the database and return all books
+  // Query the database and delete dog by their id
 
-  // Define the SQL query to get all dogs from the 'dogs' table
+  // Define the SQL query to delete dog by ID from the 'dogs' table (paramertized query)
   const querySQLText = "DELETE FROM dogs WHERE dog_id = $1 RETURNING *";
 
   // Use the pool object to send the query to the database
@@ -67,9 +67,38 @@ export async function deleteDogByID(id) {
 }
 
 export async function updateDogInformation(id, newInformation) {
+  // Query the database and edit the dogs by their id in the url
+  
+  // Define the SQL query to update dog by ID from the 'dogs' table (paramertized query)
   const queryText = `UPDATE dogs 
   SET name = $1, age = $2, date_of_birth = $3, size = $4, breed = $5 WHERE dog_id = $6 RETURNING *`;
+  
+  // parameterized large query by putting it in a array storing that in a variable
   const values = [newInformation.name, newInformation.age, newInformation.date_of_birth, newInformation.size, newInformation.breed, id];
+  
+  // Use the pool object to send the query to the database (paramertized query)
   const result = await pool.query(queryText, values);
+
+  // The rows property of the result object contains the retrieved record or null if not found
   return result.rows[0] || null;
+}
+
+export async function addNewDog(newDog) {
+  // Query the database to insert a dog into the dogs table
+
+  // Define the SQL query to insert a row into owners by ID from the 'owners' table (paramertized query)
+  const queryText =` INSERT INTO dogs
+  (name, age, date_of_birth, size, breed)
+  VALUES ($1,$2,$3,$4,$5) RETURNING *`;
+
+  // parameterized large query by putting it in a array storing that in a variable
+  const values = [newDog.name, newDog.age, newDog.date_of_birth, newDog.size, newDog.breed];
+  console.log(`values=${values}`); // testing the values 
+  
+  // Use the pool object to send the query to the database (paramertized query)
+  const result = await pool.query(queryText, values);
+  
+  // The rows property of the result object contains the retrieved record or null if not found
+  return result.rows[0] || null ;
+
 }
--- models/ownermodel.js
+++ models/ownermodel.js
@@ -1,7 +1,7 @@
 import { pool } from "../db/connection.js";
 
 export async function getOwners() {
-  // Query the database and return all books
+  // Query the database and return all owners
 
   // Define the SQL query to get all owners from the 'owners' table
   const querySQLText = "SELECT * FROM owners";
@@ -11,25 +11,27 @@ export async function getOwners() {
 
   // The rows property of the result object contains the retrieved records
   return result.rows;
+
 }
 
 export async function getOwnerByID(id) {
-  // Query the database and return all books
+  // Query the database and find an owner by theri id in the path url
 
-  // Define the SQL query to get all dogs from the 'doga' table
+  // Define the SQL query to get a specific owner from the 'owners' table (paramertized query)
   const querySQLText = "SELECT * FROM owners WHERE owner_id = $1";
 
-  // Use the pool object to send the query to the database
+  // Use the pool object to send the query to the database (paramertized query)
   const result = await pool.query(querySQLText, [id]);
 
   // The rows property of the result object contains the retrieved record or null if not found
   return result.rows[0] || null;
+
 }
 
 export async function getOwnerssAlphabetical() {
-  // Query the database and return all books
+  // Query the database and return all owners in alphabetical order
 
-  // Define the SQL query to get all dogs from the 'doga' table
+  // Define the SQL query to get all owners from the 'owners' table with a order by clause
   const querySQLText = "SELECT * FROM owners ORDER BY name ASC";
 
   // Use the pool object to send the query to the database
@@ -37,28 +39,30 @@ export async function getOwnerssAlphabetical() {
 
   // The rows property of the result object contains the retrieved records
   return result.rows;
+
 }
 
 export async function getOwnerssByName(name) {
-  // Query the database and return all books
+  // Query the database and get an owner by their name
 
-  // Define the SQL query to get all dogs from the 'doga' table
+  // Define the SQL query to get the owner by name (paramertized query)
   const querySQLText = "SELECT * FROM owners WHERE name = $1";
 
-  // Use the pool object to send the query to the database
+  // Use the pool object to send the query to the database (paramertized query)
   const result = await pool.query(querySQLText, [name]);
 
   // The rows property of the result object contains the retrieved record or null if not found
   return result.rows;
+
 }
 
 export async function deleteOwnerByID(id) {
-  // Query the database and return all books
+  // Query the database and delete owner by ID
 
-  // Define the SQL query to get all dogs from the 'doga' table
+  // Define the SQL query to delete owner by ID from the 'owners' table (paramertized query)
   const querySQLText = "DELETE FROM owners WHERE owner_id = $1 RETURNING *";
 
-  // Use the pool object to send the query to the database
+  // Use the pool object to send the query to the database (paramertized query)
   const result = await pool.query(querySQLText, [id]);
 
   // The rows property of the result object contains the retrieved record or null if not found
@@ -67,10 +71,39 @@ export async function deleteOwnerByID(id) {
 }
 
 export async function updateOwnerInformation(id, newInformation) {
-    const queryText = `UPDATE owners 
-    SET name = $1, address = $2, phone_number = $3 WHERE owner_id = $4 RETURNING *`;
-    const values = [newInformation.name, newInformation.address, newInformation.phone_number, id];
-    const result = await pool.query(queryText, values);
-    return result.rows[0] || null;
+  // Query the database and edit the the owner by their id in the url
+
+  // Define the SQL query to update owner by ID from the 'owners' table (paramertized query
+  const queryText = `UPDATE owners 
+  SET name = $1, address = $2, phone_number = $3 WHERE owner_id = $4 RETURNING *`;
+
+  // parameterized large query by putting it in a array storing that in a variable
+  const values = [newInformation.name, newInformation.address, newInformation.phone_number, id];
+    
+  // Use the pool object to send the query to the database (paramertized query)
+  const result = await pool.query(queryText, values);
+
+  // The rows property of the result object contains the retrieved record or null if not found
+  return result.rows[0] || null;
+
 }
 
+export async function addNewOwner(newOwner) {
+  // Query the database to insert an owner into the owners table
+
+  // Define the SQL query to insert a row into owners by ID from the 'owners' table (paramertized query)
+  const queryText =` INSERT INTO owners
+  (name, address, phone_number)
+  VALUES ($1,$2,$3,$4,$5) RETURNING *`;
+
+  // parameterized large query by putting it in a array storing that in a variable
+  const values = [newOwner.name, newOwner.address, newOwner.phone_number];
+  console.log(`values=${values}`); // testing the values 
+
+  // Use the pool object to send the query to the database (paramertized query)
+  const result = await pool.query(queryText, values);
+
+  // The rows property of the result object contains the retrieved record or null if not found
+  return result.rows[0] || null;
+
+}
