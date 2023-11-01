9809f8bc7e6b23331aca7d6a0988283241bca4ea
chisfy <fyzac96@gmail.com>
2023-11-01 08:45:36 +0000

    starting dom manipulation and testing frontend and backend are connected

--- client/index.js
+++ client/index.js
@@ -1,3 +1,5 @@
+async function getdogData() {
+
 // fetching the path
 const dogtable = await fetch("http://localhost:3000/dogs");
 //need to parse the data
@@ -7,3 +9,15 @@ const dogsArray = await dogs.data;
 
 console.log(dogsArray);
 
+}
+
+function test() {
+    console.log("Hello World");
+}
+
+///now need to manipulate the data to show on the html page
+const searchBar = document.querySelector("#search-name");
+
+const submitButton = document.querySelector("#submit");
+
+submitButton.addEventListener("click", test);
