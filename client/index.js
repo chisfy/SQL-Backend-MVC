6f034a307a891c736a0f83152a2dac0960cc66e9
chisfy <fyzac96@gmail.com>
2023-11-01 18:13:09 +0000

    can get all dogs

--- client/index.js
+++ client/index.js
@@ -25,7 +25,6 @@ async function retrieveDogData() {
   return dogsArray;
 }
 
-
 ///now need to manipulate the data to show on the html page
 let dogName = document.querySelector(".dog-name");
 let dogAge = document.querySelector(".dog-age");
@@ -33,9 +32,6 @@ let dogDob = document.querySelector(".dog-dob");
 let dogSize = document.querySelector(".dog-size");
 let dogBreed = document.querySelector(".dog-breed");
 let dogOwner = document.querySelector(".dog-owner");
-
-//date
-
 
 function updatingElements(dogObject) {
     dogName.textContent = `Name: ${dogObject[0]["name"]}`;
@@ -57,3 +53,38 @@ submitButton.addEventListener("click", getandretrieveDogData);
 function test() {
   console.log("Hello World");
 }
+
+
+const dogList = document.getElementById("dog-list");
+// Function to retrieve and display a list of all dogs
+async function retrieveAndDisplayAllDogs() {
+// Fetch all dog data
+const response = await fetch(`http://localhost:3000/dogs`);
+
+if (!response.ok) {
+    alert("Oh no, no dog could be found try again");
+    console.log("Oh no, no dog could be found try again");
+    console.log(`Status: ${response.status}`);
+    return;
+  }
+  const dogs = await response.json();
+  const dogsArray = await dogs.data;
+
+  // Clear existing content in the dog list
+  dogList.innerHTML = "";
+
+  // Create a list item for each dog and append it to the list
+  dogsArray.forEach(dog => {
+    const listItem = document.createElement("li");
+    listItem.textContent = `${dog.name}`;
+    dogList.appendChild(listItem);
+  });
+
+}
+
+// Add a button to the HTML that triggers the retrieval and display of all dogs
+const getAllDogsButton = document.querySelector("#get-all-dogs");
+getAllDogsButton.addEventListener("click", retrieveAndDisplayAllDogs);
+getAllDogsButton.addEventListener("dblclick", () => {
+dogList.innerHTML = "";
+});
