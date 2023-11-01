455fffd2c851a9a14475427740b642b45abc6dbe
chisfy <fyzac96@gmail.com>
2023-11-01 18:33:19 +0000

    get all dogs now on a seperate page

---
+++ client/alldogs.js
@@ -0,0 +1,29 @@
+// Function to retrieve and display a list of all dogs
+async function retrieveAndDisplayAllDogs() {
+    // Fetch all dog data
+    const response = await fetch(`http://localhost:3000/dogs`);
+
+    if (!response.ok) {
+        alert("Oh no, no dog could be found try again");
+        console.log("Oh no, no dog could be found try again");
+        console.log(`Status: ${response.status}`);
+        return;
+    }
+    const dogs = await response.json();
+    const dogsArray = await dogs.data;
+
+const dogList = document.getElementById("dog-list");
+  // Clear existing content in the dog list
+  dogList.innerHTML = "";
+
+  // Create a list item for each dog and append it to the list
+  dogsArray.forEach((dog) => {
+    const listItem = document.createElement("li");
+    listItem.textContent = `${dog.name}`;
+    dogList.appendChild(listItem);
+  });
+}
+
+// Add a button to the HTML that triggers the retrieval and display of all dogs
+const getAllDogsButton = document.querySelector("#get-all-dogs");
+document.addEventListener("DOMContentLoaded", retrieveAndDisplayAllDogs);
--- client/index.js
+++ client/index.js
@@ -53,38 +53,3 @@ submitButton.addEventListener("click", getandretrieveDogData);
 function test() {
   console.log("Hello World");
 }
-
-
-const dogList = document.getElementById("dog-list");
-// Function to retrieve and display a list of all dogs
-async function retrieveAndDisplayAllDogs() {
-// Fetch all dog data
-const response = await fetch(`http://localhost:3000/dogs`);
-
-if (!response.ok) {
-    alert("Oh no, no dog could be found try again");
-    console.log("Oh no, no dog could be found try again");
-    console.log(`Status: ${response.status}`);
-    return;
-  }
-  const dogs = await response.json();
-  const dogsArray = await dogs.data;
-
-  // Clear existing content in the dog list
-  dogList.innerHTML = "";
-
-  // Create a list item for each dog and append it to the list
-  dogsArray.forEach(dog => {
-    const listItem = document.createElement("li");
-    listItem.textContent = `${dog.name}`;
-    dogList.appendChild(listItem);
-  });
-
-}
-
-// Add a button to the HTML that triggers the retrieval and display of all dogs
-const getAllDogsButton = document.querySelector("#get-all-dogs");
-getAllDogsButton.addEventListener("click", retrieveAndDisplayAllDogs);
-getAllDogsButton.addEventListener("dblclick", () => {
-dogList.innerHTML = "";
-});
