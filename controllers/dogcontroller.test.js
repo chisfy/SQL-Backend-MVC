e111000d4af5ab6be71f5630908aa930d966e3d8
chisfy <fyzac96@gmail.com>
2024-02-02 10:40:14 +0000

    basic skeleton added and passed

---
+++ controllers/dogcontroller.test.js
@@ -0,0 +1,6 @@
+import { test } from "vitest";
+import { request } from "supertest";
+import { resetDatabase } from "../db/scripts/reset-database";
+import { app } from "../expressapp.js"
+
+test("basic skeleton test", () => {});
